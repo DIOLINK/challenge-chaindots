85a3da0ade273b514074cefd2a99d1cb562f7452
Roger BC <diolink.4103@gmail.com>
2023-09-21 10:24:01 -0300

    Error config.

--- src/views/Error/index.jsx
+++ src/views/Error/index.jsx
@@ -1,15 +1,18 @@
 import { useRouteError } from 'react-router-dom';
-
+import styles from './error.module.css';
 export const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
   return (
-    <div>
-      <h1>Oops!</h1>
+    <div className={styles.errorRoot}>
+      <h1>Oops!!!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <div className={styles.messagesError}>
+        <h4>Message Error</h4>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
 };
